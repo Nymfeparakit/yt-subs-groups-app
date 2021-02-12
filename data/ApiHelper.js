325b26a7af3ec7b71596052ef9a6cf67a09c1c15
Nymfeparakit <maxlex047@gmail.com>
2021-02-12 13:42:28 +0200

    Fix addChannelToGroupFunction

--- data/ApiHelper.js
+++ data/ApiHelper.js
@@ -1,20 +1,29 @@
 
-export const addChannelToGroup = (channelId, groupId) => {
-    fetch('http://b21376844d94.ngrok.io/channel/update-partial/' + channelId + '/', {
+export const addChannelToGroup = (channelId, channelName, groupId) => {
+    fetch('http://375417ee5b3b.ngrok.io/channels/', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({"pk": channelId, "feed_id": groupId})
+        body: JSON.stringify({
+            id: channelId,
+            name: channelName,
+            feed: groupId
+        })
     })
-    .then(response => response.json())
-    .then(data => console.log("added channel to group"))
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error(response.status)
+        }
+        return response.json()
+    })
+    .then(data => console.log(data))
 }
 
 export const getGroups = async () => {
     var groups = null
-    return await fetch('http://46e56975047d.ngrok.io/feeds/')
+    return await fetch('http://375417ee5b3b.ngrok.io/feeds/')
     .then(response => response.json())
     .then((data) => {
         return data
