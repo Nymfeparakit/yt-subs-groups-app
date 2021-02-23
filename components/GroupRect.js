7714f7f245c2d6df7629cc335c98d3c258d882bb
Nymfeparakit <maxlex047@gmail.com>
2021-02-23 13:55:16 +0300

    Fix GroupRect component

--- components/GroupRect.js
+++ components/GroupRect.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import { Text, TouchableWithoutFeedback } from 'react-native'
-import { AntDesign } from '@expo/vector-icons'
+import React from 'react';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
 
-const GroupRect = (onPress) => {
+const GroupRect = (/*{ onPress }*/) => {
     return (
         <TouchableWithoutFeedback>
-            <View flexDirection="horizontal">
+            <View flexDirection="row">
                 <Text>Group name</Text>
-                <AntDesign name="rightcircle" size={24} color="black" />
             </View>
         </TouchableWithoutFeedback>
     )
-}
+};
 
-export default GroupRect
+export default GroupRect;
