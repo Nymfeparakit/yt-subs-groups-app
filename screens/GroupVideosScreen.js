5707ea147b63925ae9d565ec9ca915458e3850c4
Nymfeparakit <maxlex047@gmail.com>
2021-02-23 13:58:21 +0300

    Start work on GroupVideosScreen.js

--- screens/GroupVideosScreen.js
+++ screens/GroupVideosScreen.js
@@ -1,5 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { getVideosForGroup } from '../data/ApiHelper';
 
-const GroupVideosScreen = () => {
+const GroupVideosScreen = ({ route }) => {
 
-}
+    const { groupId } = route.params;
+    console.log("group id: " + groupId);
+
+    const [videosList, setVideosList] = useState([]);
+    useEffect(() => {
+        getVideosForGroup(groupId).then(fetchedVideos => console.log(fetchedVideos));
+    }, []);
+
+};
+
+export default GroupVideosScreen;
