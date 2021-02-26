205d9028bdccb39c505b4a1cd59f5e6d360538c0
Nymfeparakit <maxlex047@gmail.com>
2021-02-26 09:52:23 +0200

    Show videos in list

--- screens/GroupVideosScreen.js
+++ screens/GroupVideosScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { getVideosForGroup } from '../data/ApiHelper';
+import { Image, FlatList, Text, SafeAreaView, View, StyleSheet } from 'react-native';
 
 const GroupVideosScreen = ({ route }) => {
 
@@ -8,9 +9,37 @@ const GroupVideosScreen = ({ route }) => {
 
     const [videosList, setVideosList] = useState([]);
     useEffect(() => {
-        getVideosForGroup(groupId).then(fetchedVideos => console.log(fetchedVideos));
+        getVideosForGroup(groupId).then(fetchedVideos => setVideosList(fetchedVideos));
     }, []);
 
+    const renderItem = ({ item }) => {
+        return (
+            <View>
+                <Image 
+                source={{uri: item["video_img_url"]}}
+                style={styles.videoImg}/>
+                <Text>{item["title"]}</Text>
+            </View>
+        );
+    };
+
+    return (
+        <SafeAreaView>
+            <FlatList 
+                data={videosList}
+                renderItem={renderItem}
+                keyExtractor={item => item["id"]}
+            />            
+        </SafeAreaView>
+    );
+
 };
+
+const styles = StyleSheet.create({
+        videoImg: {
+            width: 60,
+            height: 45
+        }
+});
 
 export default GroupVideosScreen;
