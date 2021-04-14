3e71e999bcf17abb47033c61b663397aeee9e6b1
Nymfeparakit <maxlex047@gmail.com>
2021-04-14 21:39:19 +0200

    edit setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-cd Distrib/ngrok;
-./ngrok http 8000;
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | \
- sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p';
+#!/bin/bash
+cd ~/Distrib/ngrok
+./ngrok http 8000 &
+ngrok_url=$(curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p')
+echo $ngrok_url
+#  cd ~/projects/yt-subs-groups-api;
+#  sed -i 's/[:alnum:]+\.ngrok\.io/'
