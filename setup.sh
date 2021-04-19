61a45f9bd1e87ec331f572de2060fa2ecc95d607
Nymfeparakit <maxlex047@gmail.com>
2021-04-19 16:35:44 +0200

    Working version of setup scipt

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 cd ~/Distrib/ngrok
-./ngrok http 8000 &
+ngrok_cmd="./ngrok http 8000"
+nohup $ngrok_cmd &
+sleep 5 # wait for ngrok starting
 ngrok_url=$(curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p')
 echo $ngrok_url
-#  cd ~/projects/yt-subs-groups-api;
-#  sed -i 's/[:alnum:]+\.ngrok\.io/'
+cd ~/projects/yt-subs-groups-api/yt_subs_groups_api
+sed -r -i "s/[[:alnum:]]+\.ngrok\.io/$ngrok_url/g" settings.py 
+cd ~/projects/yt-subs-groups-app/data
+sed -r -i "s/[[:alnum:]]+\.ngrok\.io/$ngrok_url/g" ApiHelper.js 
