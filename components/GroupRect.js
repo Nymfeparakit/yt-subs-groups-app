2a4223ef58a2bcd8609785d1031e0cde92a0d5a4
Nymfeparakit <maxlex047@gmail.com>
2021-05-07 10:18:07 +0300

    GroupRect.js: Adding styles

--- components/GroupRect.js
+++ components/GroupRect.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import { Text, TouchableWithoutFeedback, View, TouchableOpacity } from 'react-native';
+import { Text, TouchableWithoutFeedback, View, TouchableOpacity, StyleSheet } from 'react-native';
 
 const GroupRect = ({ title, id, onPress }) => {
     return (
         <TouchableOpacity
         onPress={() => onPress(id)}>
-            <View flexDirection="row">
-                <Text>{ title }</Text>
+            <View flexDirection="row" style={styles.container}>
+                <Text style={styles.title}>{ title }</Text>
             </View>
         </TouchableOpacity>
     )
 };
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 20
+    },
+    container: {
+        borderBottomWidth: 1,
+        paddingVertical: 10,
+        paddingLeft: 5
+    }
+});
 
 export default GroupRect;
