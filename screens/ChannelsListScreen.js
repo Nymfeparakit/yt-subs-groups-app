e426f68ab88da643812131bc122479a0f7da9b5e
Nymfeparakit <maxlex047@gmail.com>
2021-05-07 11:26:50 +0300

    fix(ChannelsListScreen.js): Use useFocusEffect hook instead of useIsFocused

--- screens/ChannelsListScreen.js
+++ screens/ChannelsListScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { View, FlatList, StyleSheet, TouchableOpacity, Image, Text, Touchable } from 'react-native'
 import Faker from 'faker'
 import { AntDesign } from '@expo/vector-icons'
@@ -6,7 +6,8 @@ import { getChannels } from '../data/ApiHelper'
 import { ActivityIndicator } from 'react-native-paper'
 import { ScrollView } from 'react-native-gesture-handler'
 import { Accordion } from 'native-base';
-import { useIsFocused } from '@react-navigation/native'
+import { useFocusEffect } from '@react-navigation/native'
+import { useCallback } from 'react'
 
 const ChannelsListScreen = ({ navigation }) => {
 
@@ -14,7 +15,10 @@ const ChannelsListScreen = ({ navigation }) => {
     const [selectedIds, setSelectedIds] = useState([])
     const [expanded, setExpanded] = useState(true)
     const [accordionDataArray, setAccordionDataArray] = useState([]);
-    const isFocused = useIsFocused();
+    // const isFocused = useMemo(() => {
+    // const isFocused = useIsFocused();
+    // return isFocused;
+    // }, []);
     const [loading, setLoading] = useState(true);
 
     const handlePress = () => setExpanded(!expanded)
@@ -34,21 +38,23 @@ const ChannelsListScreen = ({ navigation }) => {
 
     };
 
-    useEffect(() => {
-        setLoading(true);
-        getChannels()
-            .then(successCallback)
-            .catch(err => console.log("failed with promise again: " + err.message));
-        setSelectedIds([]);
-        // tmpChannels = [];
-        // for (var i = 0; i < 10; ++i) {
-        //     tmpChannels.push({
-        //         'title': Faker.name.findName(),
-        //         'icon_url': Faker.image.cats()
-        //     })
-        // }
-        // setChannels(tmpChannels);
-    }, [isFocused]); // fetch data every time screen is focused again
+    useFocusEffect(
+        useCallback(() => {
+            setLoading(true);
+            getChannels()
+                .then(successCallback)
+                .catch(err => console.log("failed with promise again: " + err.message));
+            setSelectedIds([]);
+            // tmpChannels = [];
+            // for (var i = 0; i < 10; ++i) {
+            //     tmpChannels.push({
+            //         'title': Faker.name.findName(),
+            //         'icon_url': Faker.image.cats()
+            //     })
+            // }
+            // setChannels(tmpChannels);
+        }, [])
+    ); // fetch data every time screen is focused again
 
     useEffect(() => {
         var accordionDataArrayTmp = [];
@@ -64,7 +70,7 @@ const ChannelsListScreen = ({ navigation }) => {
         console.log("accordion data array was set");
     }, [channels, selectedIds]);
 
-    const renderAccordionContent = ( content ) => {
+    const renderAccordionContent = (content) => {
         return (
             <FlatList
                 data={content}
@@ -73,16 +79,16 @@ const ChannelsListScreen = ({ navigation }) => {
 
                     return (
                         <TouchableOpacity
-                        onPress={() => onChannelPress(item["id"])}
+                            onPress={() => onChannelPress(item["id"])}
                         >
-                        <View style={[{backgroundColor: channelBgColor}, styles.channelView]}>
-                        {/* <View style={styles.channelView}> */}
-                            <Image
-                                source={{ uri: item["icon_url"] }}
-                                style={{ width: 50, height: 50 }}
-                            />
-                            <Text>{item["title"]}</Text>
-                        </View>
+                            <View style={[{ backgroundColor: channelBgColor }, styles.channelView]}>
+                                {/* <View style={styles.channelView}> */}
+                                <Image
+                                    source={{ uri: item["icon_url"] }}
+                                    style={{ width: 50, height: 50 }}
+                                />
+                                <Text>{item["title"]}</Text>
+                            </View>
                         </TouchableOpacity>
                     );
                 }
@@ -102,17 +108,17 @@ const ChannelsListScreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
             {
-                loading 
-                ? <ActivityIndicator size="large" />
-                : <ScrollView>
-                    <Accordion
-                        dataArray={accordionDataArray}
-                        expanded={[-1]}
-                        renderContent={(accordionDataArray) => renderAccordionContent(accordionDataArray.content)}
-                        renderHeader={(accordionDataArray) => renderAccordionHeader(accordionDataArray.title)}
-                    />
-                </ScrollView>
-            }       
+                loading
+                    ? <ActivityIndicator size="large" />
+                    : <ScrollView>
+                        <Accordion
+                            dataArray={accordionDataArray}
+                            expanded={[-1]}
+                            renderContent={(accordionDataArray) => renderAccordionContent(accordionDataArray.content)}
+                            renderHeader={(accordionDataArray) => renderAccordionHeader(accordionDataArray.title)}
+                        />
+                    </ScrollView>
+            }
             {
                 !loading && typeof selectedIds !== 'undefined' && selectedIds.length > 0 ?
                     <TouchableOpacity
