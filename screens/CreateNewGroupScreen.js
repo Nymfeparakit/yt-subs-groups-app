4bcce192efe130f7624d6f625fd458f07dd0b6e6
Nymfeparakit <maxlex047@gmail.com>
2021-05-07 10:20:21 +0300

    CreateNewGroupScreen.js: Remove commented code

--- screens/CreateNewGroupScreen.js
+++ screens/CreateNewGroupScreen.js
@@ -4,18 +4,6 @@ import { createNewFeed } from '../data/ApiHelper'
 
 const CreateNewGroupScreen = () => {
 
-    // const createFeed = (newFeed) => {
-    //     fetch('http://c3dce0184757.ngrok.io/feeds/', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Accept': 'application/json',
-    //             'Content-Type': 'application/json' 
-    //         },
-    //         body: JSON.stringify({'name': newFeed})
-    //     }).then(response => response.json())
-    //     .then(data => console.log("posted new feed"))
-    // }
-
     const [groupName, setGroupName] = useState('')
 
     return (
